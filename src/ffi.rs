9ca1eb47ddb39b762254b4d13b6e114bd2f245c5
KeeganBeuthin <kdbeuthin@gmail.com>
2024-09-05 23:44:07 +0200

    subtle

--- src/ffi.rs
+++ src/ffi.rs
@@ -3,14 +3,6 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern "C" fn alloc(len: usize) -> *mut u8 {
-    let mut buf = Vec::with_capacity(len);
-    let ptr = buf.as_mut_ptr();
-    std::mem::forget(buf);
-    ptr
-}
-
-#[no_mangle]
 pub extern "C" fn dealloc(ptr: *mut u8, len: usize) {
     unsafe {
         let _ = Vec::from_raw_parts(ptr, 0, len);
